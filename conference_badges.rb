3bc5582a1b843f01f9072e1911f74bf82c3c0ead
Amy Fong <amyhfong@gmail.com>
2019-08-02 21:04:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(list)
 end
 
 def assign_rooms(list)
-    list.each_with_index.collect{|name, index|
+    list.each_with_index.collect {|name, index|
       "Hello, #{name}! You'll be assigned to room #{index+1}!"
     }
 end
